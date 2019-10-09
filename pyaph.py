6a684a8cc45cf0ba4c595562ec47c38ecaabfa35
HimaJyun <HimaJyun@users.noreply.github.com>
2019-10-10 02:49:15 +0900

    一応使えるようにした

--- pyaph.py
+++ pyaph.py
@@ -11,19 +11,21 @@ from pathlib import Path
 
 def plot(args: argparse.Namespace):
     df = None
-    if args.type == "csv":
-        df = pd.read_csv(args.input,encoding="utf-8",index_col=0)
-    elif args.type == "tsv":
-        pass
+    # ArgParseのFileTypeを使うとエンコーディングが上手くいかないのでこうする
+    if args.input == "-":
+        df = pd.read_csv(sys.stdin, index_col=0)
+    else:
+        df = pd.read_csv(args.input, encoding=args.encoding, index_col=0)
     print(df)
-    plt.rcParams["font.family"] = "IPAexGothic"
     df.plot()
-    plt.ylabel("ﾟC",rotation=0)
-    plt.show()
+
+    if "ylabel" in args:
+        plt.ylabel(args.ylabel,rotation=args.yrotation)
+    show(args.output)
 
 
-def show(file: Path = None):
-    if file is None:
+def show(file: str):
+    if file == "-":
         plt.show()
     else:
         plt.savefig(file)
@@ -35,20 +37,29 @@ def main():
     sub_parser = parser.add_subparsers()
 
     sub = sub_parser.add_parser("plot")
-    sub.add_argument("-t", "--type", choices=["csv", "tsv", "json"],default="csv")
-    sub.add_argument("input",
-                     nargs="?",
-                     type=argparse.FileType("r"),
-                     default=sys.stdin)
-    sub.add_argument("output",nargs="?",type=str,default="-")
+    #sub.add_argument("-t",
+    #                 "--type",
+    #                 choices=["csv", "tsv", "json"],
+    #                 default="csv")
+    sub.add_argument("-e", "--encoding", default="utf-8")
+    sub.add_argument("-y", "--ylabel", type=str)
+    sub.add_argument("--yrotation", type=int, default=90)
+    sub.add_argument("input", nargs="?", type=str, default="-")
+    sub.add_argument("output", nargs="?", type=str, default="-")
     sub.set_defaults(func=plot)
 
     args = parser.parse_args()
     # set seaborn style
     sns.set()
     sns.set_style("whitegrid", {"grid.linestyle": "--"})
+    plt.rcParams["font.family"] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = [
+        'Hiragino Maru Gothic Pro', 'Yu Gothic', 'Meirio', 'Takao',
+        'IPAexGothic', 'IPAPGothic', 'VL PGothic', 'Noto Sans CJK JP'
+    ]
     # 実行
     args.func(args)
+
 
 if __name__ == "__main__":
     main()
