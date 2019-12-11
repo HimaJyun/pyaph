57bc3feba3d69a559979c6c44603450e8d91252c
HimaJyun <HimaJyun@users.noreply.github.com>
2019-12-11 19:32:29 +0900

    複数の値に対応

--- convert.py
+++ convert.py
@@ -27,6 +27,7 @@ def date2sec(day: str, time: str) -> float:
 
 
 def read(reader):
+    #　ヘッダー飛ばし
     next(reader)
     result = []
 
@@ -47,55 +48,55 @@ def read(reader):
 
 def single(file: str):
     with open(file) as f:
-        reader = csv.reader(f)
-        next(reader)
-        d = read(reader)
         print(" 秒 ,温度")
-        for v in d:
+        for v in read(csv.reader(f)):
             print(f"{v[0]},{v[1]}")
 
 
-def double(file1: str, file2: str):
-    with open(file1) as f1, open(file2) as f2:
-        r1 = csv.reader(f1)
-        r2 = csv.reader(f2)
-        next(r1)
-        next(r2)
-        t1 = read(r1)
-        t2 = read(r2)
+def multi(files: list):
+    reader = []
+    # 指定されたファイルを全て読む
+    for file in files:
+        with open(file) as f:
+            reader.append(read(csv.reader(f)))
 
-        d = []
-        for v in t1:
-            d.append({"time": v[0], "temp1": v[1]})
-        for v in t2:
-            d.append({"time": v[0], "temp2": v[1]})
-        s = sorted(d, key=lambda x: x["time"])
+    # 辞書配列に入れてソート
+    d = []
+    value = []
+    for i, j in enumerate(reader):
+        value.append(None)
+        for v in j:
+            d.append({"index": i, "time": v[0], "temp": v[1]})
+    s = sorted(d, key=lambda x: x["time"])
 
-        v1 = None
-        v2 = None
-        print(" 秒 ,温度1,温度2")
-        for v in s:
-            if "temp1" in v:
-                v1 = v["temp1"]
-            if "temp2" in v:
-                v2 = v["temp2"]
-
-            if (v1 is not None) and (v2 is not None):
-                t = v["time"]
-                print(f"{t},{v1},{v2}")
+    # ヘッダー
+    print(" 秒 ", end="")
+    for i in range(len(value)):
+        print(f",温度{i+1}", end="")
+    print("")
+    # 出力
+    for v in s:
+        value[v["index"]] = v["temp"]
+        # Noneが含まれるなら値がまだ
+        if None in value:
+            continue
+        print(v["time"], end="")
+        for n in value:
+            print(f",{n}", end="")
+            pass
+        print("")
 
 
 def main():
-    if len(sys.argv) == 0:
-        print(f"Usage: {sys.argv[0]} <single/double> [file]")
+    if len(sys.argv) <= 1:
+        print(f"Usage: {sys.argv[0]} [file...]")
         sys.exit(1)
 
-    if sys.argv[1] == "single":
-        single(sys.argv[2])
-    elif sys.argv[1] == "double":
-        double(sys.argv[2], sys.argv[3])
-    else:
+    if len(sys.argv) == 2:
         single(sys.argv[1])
+    else:
+        sys.argv.pop(0)
+        multi(sys.argv)
 
 
 if __name__ == "__main__":
